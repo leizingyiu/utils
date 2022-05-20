36c21a8d3f85ad39ab27287852782c710c8a375e
leizingyiu <youcanfindme@126.com>
2022-05-21 04:31:48 +0800

    Update toTitleUpperCase.js

--- js/toTitleUpperCase.js
+++ js/toTitleUpperCase.js
@@ -4,3 +4,5 @@ String.prototype.toTitleUpperCase = function () {
     return this.replace(/\S+/g, (word, idx, sentence) => ['the', 'a', 'an'].indexOf(word) != -1 ? word : word.toLowerCase().replace(/^[a-zA-Z]/, letter => letter.toUpperCase()));
 }
 console.log('The quick brown fox jumps over the lazy dog', ' => ', 'The quick brown fox jumps over the lazy dog'.toTitleUpperCase());
+//import {toTitleUpperCase} from "https://cdn.jsdelivr.net/gh/leizingyiu/utils@master/js/toTitleUpperCase.js"
+// <script src="https://cdn.jsdelivr.net/gh/leizingyiu/utils@master/js/toTitleUpperCase.js"></script>
